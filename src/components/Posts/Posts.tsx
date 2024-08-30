d1db5c300a2a30dea6d55bee6caf506281563de8
Sanjeewani Bhoite <sanjeewanibhoite@gmail.com>
2024-08-31 00:10:41 +0530

    card added

--- src/components/Posts/Posts.tsx
+++ src/components/Posts/Posts.tsx
@@ -2,6 +2,7 @@ import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchPosts } from "../../utilities/redux/slices/postSlice";
 import { RootState } from "../../utilities/redux/store/store";
+import Card from "../Card/card";
 
 const Posts = () => {
   const dispatch = useDispatch();
@@ -13,7 +14,12 @@ const Posts = () => {
     }
   }, [postStatus, dispatch]);
 
-  return <div>Posts</div>;
+  return <div>
+
+<h3>POSTS</h3>
+<Card />
+
+  </div>;
 };
 
 export default Posts;
---
+++ src/themes/colors.ts
@@ -0,0 +1,6 @@
+export const colors = {
+  WHITE: "#FCFCFC",
+  BLACK: "#110E18",
+  ORANGE: "#FF8600",
+  PURPLE: "#6E44FF",
+};
